b01371d14b41767b197ba4a997e0d6930ad002c6
Ricard Garcia <imricardgarcia@gmail.com>
2021-07-21 14:25:46 +0200

    feat: added comment

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -4,6 +4,7 @@ class ErrorController extends Controller
 {
     function __construct()
     {
+        echo "<p>Loaded ErrorController</p>";
         echo "<p style='color: red'>ERROR, CONTROLLER NOT FOUND</p>";
     }
 }
