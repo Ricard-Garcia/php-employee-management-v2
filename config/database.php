2eaf097af97f2afe8b69cf89aad2f5ca888a80fe
Ricard Garcia <imricardgarcia@gmail.com>
2021-07-21 10:18:38 +0200

    feat: added extra spaces

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 // Defining constants for db connection
 
 define("HOST", "http://localhost:8888/20.1-Php-Employee-Management-v2/");
+
 define("DB", "emp_man_v2");
+
 define("USER", "root");
+
 define("PASSWORD", "root");
